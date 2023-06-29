37bec6f7cf4702266c6b96ac165ef2d59142826a
Joi Misenti <joipolloi1@gmail.com>
2023-06-29 13:26:36 -0400

    breaking for lunch

--- starter.js
+++ starter.js
@@ -5,26 +5,29 @@
 const lotr = ['biLbO BaGGINs', 'leGOlAs', 'Frodo bAGGINS', 'sAMwiSe GamGEe', 'gAndALF tHe GREY']
 
 const copyArrToCamelCase = arr => {
-    const newArr = []
+    const newArr = [] // instantiating a new array (most likely to return it)
 
-    for (let i = 0; i < arr.length; i++) {
-        const str = arr[i]
-        const splitStr = str.split(' ')
+    for (let i = 0; i < arr.length; i++) { // starting a for loop using the indices
+        const str = arr[i] // 1st it: str = 'biLbO BaGGINs'
+        const splitStr = str.split(' ') // splitStr = ['biLbO', 'BaGGINs'] // splitting it on the space, each element becoming a new part of the array
         let camelCaseStr = ''
         
-        for (let x = 0; x < splitStr.length; x++) {
-            let word = splitStr[x]
+        for (let x = 0; x < splitStr.length; x++) { // nested for-loop using indices --
+            let word = splitStr[x] // word = 'biLbO'
 
-            word = word.toLowerCase()
+            word = word.toLowerCase() // word = 'bilbo' // 'baggins'
 
-            if (x !== 0) {
-                word = word.charAt(0).toUpperCase() + word.slice(1)
+            if (x !== 0) { // 1st word in camelCase is all lower, so at this poinst, if x === 0 then we are looking at the first word of our camelCase conversion
+                word = word.charAt(0).toUpperCase() + word.slice(1) //'b' --> 'B' + 'aggins' --> 'Baggins'
+                // word = 'B' + word.slice(1)
+                // word = 'B' + 'aggins'
+                // word = 'Baggins'
             }
 
-            camelCaseStr += word
+            camelCaseStr += word // = 'bilbo' // = 'bilboBaggins'
         }
 
-        newArr.push(camelCaseStr)
+        newArr.push(camelCaseStr) // newArr = ['bilboBaggins']
     }
 
     return newArr
@@ -45,6 +48,54 @@ const copyArrToSnakeCase = arr => {
 }
   
 // CODE HERE
+
+// 4: Now let's imagine that we are thinking ahead (you'll be able to someday), and we realize that these functions aboce can be simplified and used as "callback" functions, if we have an outer function that can take in an array and a callback.
+
+// Logic: If we can get a callback function to modify each value in the array, then we can iterate through the array confidently call on the callback and use its return at each iteration
+
+// Want outer function to iterate array, make a copy, and return the copy
+const copyArrAndChange = (array, callback) => {
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        let newValue = callback(arr[i]) // we don't know what it will do yet, depends on what the callback ends up being
+        result.push(newValue)
+    }
+    return result
+}
+
+// With this outer function, we can apply any callback that will work with the data type housed in the array given as the argument. So we can make all kinds of callback functions and use them as needed in this function --->
+
+// Callback to copy a string to snake_case:
+// Logic: Outer function should call on this function pasing in a string only. Therefore we need only take the string and convert it to snake_case and return it
+
+const copyStrToSnakeCase = (str) => {
+    str = str.toLowerCase()
+    const splitStr = str.split(' ')
+    const snakeCaseStr = splitStr.join('_')
+    return snakeCaseStr
+}
+
+console.log(copyStrToSnakeCase("bilbO baggins"))
+console.log(copyArrAndChange(lotr, copyStrToSnakeCase))
+
+// 6: CHALLENGE: Make a callback function that takes in a number and returns that number multiplied by 4. Try with arrow function syntax. Can you do it in one line?
+
+const multByFour = (aNum) => aNum * 4
+
+// const multByFour = aNum => {
+//     return aNum * 4
+// }
+
+// const multByFour = aNum => {return aNum * 4}
+
+let nums = [1, 2, 3, 4, 5]
+
+console.log(copyArrAndChange(nums, multByFour))
+console.log(nums) // nums array did not actually change
+
+console.log(nums.map(multByFour))
+
+console.log(nums.map((num) => num * 4))
 
 
 ////////////////////////////////////////
